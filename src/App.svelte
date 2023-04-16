28d5a71f48e01944ab83e6cd3a4919fc548061cd
@racascou <rafaelcastrocouto@gmail.com>
2023-04-16 09:32:14 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,66 @@
+<script>
+  import logo from './assets/weird.png'
+  import svelte from './assets/svelte.png'
+  import Counter from './lib/Team.svelte'
+  import Github from './lib/server/Github.svelte'
+</script>
+
+<main>
+  <img src={logo} alt="Weird Logo" class="logo_img"/>
+  <h1>Weird Team!</h1>
+
+  <Github />
+  
+  <!-- Counter /-->
+
+  <p class="footer">
+    Made with  <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a>, also powered by <a href="https://vitejs.dev/" target="_blank"></a>Vite!
+  </p>
+  
+  <a href="https://svelte.dev" target="_blank"><img src={svelte} alt="Svelte Logo" class="footer_img"/></a>
+  
+  <p>
+     Visit <a href="https://svelte.dev" target="_blank">svelte.dev</a> to learn how to build Svelte apps.
+  </p>
+
+  
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    max-width: 30rem;
+  }
+
+  .logo_img {
+    height: 16rem;
+    width: 16rem;
+  }
+
+  h1 {
+    color: var(--orange);
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 0;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  .footer {
+    margin-top: 4em;
+  }
+
+  .footer_img {
+    height: 6rem;
+    width: 6rem;
+  }
+
+</style>
---
+++ src/lib/server/Github.svelte
@@ -0,0 +1,97 @@
+<script>
+  const orgs = ['spicylobstergames', 'fishfolk'/*, 'commune-os'*/];
+
+  async function githubJson (url) {
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+  }
+
+  async function userData (username) {
+    const user_url ='https://api.github.com/users/';
+    const json = await githubJson(user_url+username);
+    //return JSON.stringify(json);
+    return json;
+  }
+  //const data = userData('rafaelcastrocouto');
+
+  async function userDataList (members) {
+    const promises = members.map(userData);
+    const json = await Promise.all(promises);
+    //return JSON.stringify(json);
+    return json;
+  }
+  //const data = userDataList(['orhun','rafaelcastrocouto','erlend-sh']);
+
+  async function orgData (orgname) {
+    const org_url = 'https://api.github.com/orgs/';
+    const json = await githubJson(org_url+orgname+'/members');
+    //return JSON.stringify(json);
+    return json;
+  }
+  //const data = orgData('fishfolk');
+  
+  async function orgDataList (org) {
+    const teamJSON = await orgData(org);
+    const list = teamJSON.map( (user) => user.login )
+    return list;
+  }
+  //const data = orgDataList('fishfolk');
+
+  async function uniqueList (orgs) {
+    const promises = orgs.map(orgDataList);
+    const list = await Promise.all(promises);
+    const unique = Array.from(new Set([].concat( ...list )));
+    return unique;
+  }
+  //const data = uniqueList(['spicylobstergames', 'fishfolk']);
+
+  async function mergeTeams (orgs) {
+    const everyone = await uniqueList(orgs);
+    const json = await userDataList(everyone);
+    //return JSON.stringify(json);
+    return json
+  }
+
+  const data = mergeTeams(orgs);
+</script>
+
+<div class="members">
+  {#await data then users}
+    {#each users as user}
+      <div class="member">
+        <a href="{user.html_url}" target="_blank"><img src={user.avatar_url}></a>
+        <h4>
+        {#if user.twitter_username} 
+          <a href="https://twitter.com/{user.twitter_username}" target="_blank">@{user.twitter_username}</a> 
+        {:else}
+          <a href="{user.html_url}" target="_blank">{user.login}</a>
+        {/if}
+        </h4>
+        {#if user.blog} <p><a href="{user.blog}" target="_blank">{user.blog}</a></p> {/if}
+        {#if user.company} <p>{user.company}</p> {/if}
+        {#if user.location} <p>{user.location}</p> {/if}
+        {#if user.bio} <p>{user.bio}</p> {/if}
+      </div>
+    {/each}
+  {/await}
+</div>
+
+<style>
+  img {
+    border-radius: 50%;
+    margin: 3rem 0 0;
+    height: 6rem;
+    width: 6rem;
+  }
+  a {
+    color: var(--orange);
+  }
+  h4 {
+    color: var(--orange);
+    margin: 0.5rem 0 0;
+  }
+  p {
+    margin: 0.5rem 0 0;
+  }
+</style>
